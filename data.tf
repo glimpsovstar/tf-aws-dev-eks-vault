e4eceebb0b13e0c885038dc7bfc78bde80430ab1
David Joo <david.joo@hashicorp.com>
2025-03-12 18:58:16 +1100

    Fixed missing aws_eks_cluster_auth reference in provider.tf

--- data.tf
+++ data.tf
@@ -4,8 +4,11 @@ data "terraform_remote_state" "eks" {
   config = {
     organization = "djoo-hashicorp"
     workspaces = {
-      name = "tf-aws-eks"
+      name = "tf-aws-dev-eks"
     }
   }
 }
 
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = data.terraform_remote_state.eks.outputs.eks_cluster_name
+}
--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  cloud {
+    organization = "djoo-hashicorp"
+    workspaces {
+      name = "tf-aws-dev-eks-vault"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -7,4 +16,3 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.eks_auth.token
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.eks_certificate_authority)
 }
-
