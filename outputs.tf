1df85c96c37fc3a31805789861ad569aad6f301e
David Joo <david.joo@hashicorp.com>
2025-03-12 21:01:21 +1100

    Removed reference to non-existent vault_certificate resource

--- outputs.tf
+++ outputs.tf
@@ -8,11 +8,6 @@ output "vault_storage_pvc" {
   value       = kubernetes_persistent_volume_claim.vault_storage.metadata.0.name
 }
 
-output "vault_tls_secret" {
-  description = "Secret name containing Vault's TLS certificate"
-  value       = kubernetes_manifest.vault_certificate.manifest.spec.secretName
-}
-
 output "vault_root_token_secret" {
   description = "Kubernetes secret storing Vault root token"
   value       = kubernetes_secret.vault_root_token.metadata.0.name
